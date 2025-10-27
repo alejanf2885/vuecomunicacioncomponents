b0bbd302ff30f1bdb19a6deff70bf3394b864136
alejanf2885 <alumno.648000@ies-azarquiel.es>
2025-10-27 14:15:01 +0100

    propsComponents

---
+++ src/components/HijoDeportes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hijo deporte</h1>
+    <p>{{ nombredeporte }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HijoDeportes',
+    props: ['nombredeporte']
+   
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PadeDeportes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Padre Deporte</h1>
+    <ul>
+        <li v-for="deporte in deportes" :key="deporte">
+            <HijoDeportes :nombredeporte="deporte" />
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HijoDeportes from './HijoDeportes.vue';
+
+export default {
+    name:'PadreDeportes',
+    components: {
+        HijoDeportes
+    },
+     data() {
+        return{
+            deportes: ['futbol','voley','tenis','atletismo']
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
